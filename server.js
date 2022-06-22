66fcdc06c34c603d28b10fb8d20c12ab4a723d02
noothanks <coreyfuller727@yahoo.com>
2022-06-22 12:54:42 -0400

    adding POST method and validating data

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+//parse incoming json data
+app.use(express.json());
 const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalsArray) {
@@ -44,6 +49,31 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    //main code
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray}, null, 2)
+    );
+    //return finished code to post route
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -57,6 +87,20 @@ app.get('/api/animals/:id', (req, res) => {
         res.json(result);
     } else {
         res.send(404)
+    }
+});
+app.post('/api/animals', (req, res) => {
+    //set animal id based on the next index of the animals array
+    req.body.id = animals.length.toString();
+    
+    //if any data is invalid, send 400 error
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    }
+    //otherwise add animal to json file and animals array
+    else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(req.body);
     }
 });
 app.listen(PORT, () => {
